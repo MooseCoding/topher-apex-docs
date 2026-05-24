4863bc71d9198b5081351c61df34fd14809dd91e
Xander-r343 <Xhaemel@gmail.com>
2026-05-23 20:49:12 -0700

    Swerve Offsets class

--- app/docs/tuning/_meta.js
+++ app/docs/tuning/_meta.js
@@ -3,6 +3,6 @@ export default {
     "heading-tuner": "Heading Tuner",
     "lateral-tuner": "Lateral Tuner",
     "axial-tuner": "Axial Tuner",
-    "teleop-test": "Teleop Test"
-
+    "teleop-test": "Teleop Test",
+    "swerve-offsets": "Swerve Offsets",
 }
