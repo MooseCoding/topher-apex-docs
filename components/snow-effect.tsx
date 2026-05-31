cd84300b2e8f1d361b3a77ebd462421d841d51b8
sohum-arora <sohumarora28@gmail.com>
2026-05-31 22:10:51 +0530

    Snow effects fixed now hopefully

--- components/snow-effect.tsx
+++ components/snow-effect.tsx
@@ -6,18 +6,19 @@ import Snowfall from "react-snowfall";
 export default function SnowEffect() {
   return (
     <Snowfall
+      // Brought back to the foreground, but allows clicks to pass through
       style={{
         position: "fixed",
         width: "100vw",
         height: "100vh",
-        zIndex: -1, // Pushes the snow behind your page content
+        zIndex: 10,
+        pointerEvents: "none",
       }}
-      // Optional configuration props to tweak the appearance
       snowflakeCount={100} // Total number of snowflakes
-      color="#ffffff"      // Color of the snow
-      speed={[0.5, 2.5]}   // Min and max vertical speed [min, max]
-      wind={[-0.5, 1.5]}   // Min and max horizontal drift [min, max]
-      radius={[0.5, 3.0]}  // Min and max snowflake size in pixels
+      color="#ffffff"      // Color of the snow[cite: 1]
+      speed={[1.0, 3.2]}   // Slightly faster vertical speed (was [0.5, 2.5])
+      wind={[0.5, 2.5]}    // Pushed rightward for a more noticeable slant (was [-0.5, 1.5])
+      radius={[0.5, 3.0]}  // Min and max snowflake size in pixels[cite: 1]
     />
   );
 }
