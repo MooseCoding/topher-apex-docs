3f8be5045682c33a3031c88708d1af24992513d7
paradoxical-duck <sohumarora28@gmail.com>
2026-06-11 00:06:57 +0530

    FAHHHHHHHHHHHHHH
    FAHHHHHHHHHHHHHHH

--- app/docs/tuning/_meta.js
+++ app/docs/tuning/_meta.js
@@ -1,5 +1,5 @@
 export default {
-    "follower-tuner" : "Follower Tuner"
+    "follower-tuner" : "Follower Tuner",
     "swerve-offsets": "Swerve Offsets",
     "teleop-test": "Teleop Test",
     "auto-test": "Auto Test"
