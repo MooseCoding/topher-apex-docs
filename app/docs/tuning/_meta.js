92e7a7f3c2260b361f515b39ae88bc5aef727ff6
sohum-arora <sohumarora28@gmail.com>
2026-06-10 13:15:46 +0530

    Fix Meta.Js

--- app/docs/tuning/_meta.js
+++ app/docs/tuning/_meta.js
@@ -1,9 +1,5 @@
 export default {
-    "overview": "Overview",
-    "heading-tuner": "Heading Tuner",
-    "lateral-tuner": "Lateral Tuner",
-    "axial-tuner": "Axial Tuner",
+    "follower-tuner" : "Follower Tuner"
     "swerve-offsets": "Swerve Offsets",
-    "teleop-test": "Teleop Test",
-    "bspline-tuner": "BSpline Tuner"
+    "teleop-test": "Teleop Test"
 }
